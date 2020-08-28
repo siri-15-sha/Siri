2fed886107d31c4d707f1a8e2bd2897c16879673
siri-15-sha <70366514+siri-15-sha@users.noreply.github.com>
2020-08-28 16:47:40 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class Add {
+	public static void main(String[] args){
+		int a=10;
+		int b=20;
+		int c=30;
+		int sum=a+b+c;
+		System.out.println("sum = "+sum);
+	}
+
+}
