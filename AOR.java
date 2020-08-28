29d62c5a835c9ad4392d11a767e867486ded4ea4
siri-15-sha <70366514+siri-15-sha@users.noreply.github.com>
2020-08-28 16:48:00 +0530

    Add files via upload

---
+++ AOR.java
@@ -0,0 +1,20 @@
+package pack1;
+import java.util.*;
+
+public class AOR {
+
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Length= ");
+		System.out.println("Breadth= ");
+		int l=obj.nextInt();
+		int b=obj.nextInt();
+		int a=l*b;
+		System.out.println("Area of Rectangle: "+a);
+		
+		
+		
+
+	}
+
+}
