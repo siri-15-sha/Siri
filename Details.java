96be5d688ebcc826d78a8a8d02cb2a1122c60e85
siri-15-sha <70366514+siri-15-sha@users.noreply.github.com>
2020-08-30 17:04:44 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,23 @@
+package pack2;
+import java.util.*;
+public class Details {
+
+	public static void main(String[] args) {
+		Scanner obj=new Scanner (System.in);
+		System.out.println("Enter Name:");
+		String Name=obj.next();
+		System.out.println("Enter Age:");
+		int Age=obj.nextInt();
+		System.out.println("Enter Gender:");
+		String Gender=obj.next();
+		System.out.println("Enter Place:");
+		String Place=obj.next();
+		System.out.println("Details are:");
+		System.out.println("Name:"+Name+"\n Age:"+Age+"\n Gender:"+Gender+"\n Place:"+Place);
+		
+		
+		
+
+	}
+
+}
