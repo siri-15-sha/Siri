8d1aafba9618f8394b7cad98295681e6267a1e8b
siri-15-sha <70366514+siri-15-sha@users.noreply.github.com>
2020-08-30 17:05:08 +0530

    Add files via upload

---
+++ Even_Odd.java
@@ -0,0 +1,18 @@
+package pack2;
+import java.util.*;
+
+public class Even_Odd {
+
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enter number:");
+		int a=obj.nextInt();
+		if(a%2==0)
+		System.out.println("Given no. is EVEN");
+		else
+			System.out.println("Given no. is ODD");
+		
+
+	}
+
+}
