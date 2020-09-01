6537219c46f605f13a6af3836992bb2f92535b17
siri-15-sha <70366514+siri-15-sha@users.noreply.github.com>
2020-09-01 09:33:57 +0530

    Add files via upload

---
+++ Loop_D.java
@@ -0,0 +1,19 @@
+package pack2;
+import java.util.*;
+
+public class Loop_D {
+
+	public static void main(String[] args) {
+		System.out.println("5 TABLE:");
+		int n=5;
+		int i=1;
+		while (i<=10)
+		{
+			System.out.println(n+"*"+i+"="+(n*i));
+			i=i+1;
+		}
+		
+
+	}
+
+}
