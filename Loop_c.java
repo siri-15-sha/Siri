1a9055734e8c1c7cb776c2307012ba9c401e7f30
siri-15-sha <70366514+siri-15-sha@users.noreply.github.com>
2020-09-01 09:32:37 +0530

    Add files via upload

---
+++ Loop_c.java
@@ -0,0 +1,17 @@
+package pack2;
+import java.util.*;
+
+public class Loop_c {
+
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		int i=1;
+		while (i<=10)
+		{
+			System.out.print("\t"+i);
+			i=i+1;
+		}
+
+	}
+
+}
